28e9e04685a7d1b9f6ba8510859e8ed2b0ffe772
giangnguyen99 <ronaldo0x2cr7@gmail.com>
2021-03-11 16:56:54 +0700

    cohesion

--- src/main/java/views/screen/shipping/ShippingScreenHandler.java
+++ src/main/java/views/screen/shipping/ShippingScreenHandler.java
@@ -27,7 +27,7 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
-
+// Temporal cohesion: Vì các phương thức sắp xếp thực hiện theo thứ tự thời gian với công việc Ship
 public class ShippingScreenHandler extends BaseScreenHandler {
 
 	private static final Logger LOGGER = Utils.getLogger(ShippingScreenHandler.class.getName());
